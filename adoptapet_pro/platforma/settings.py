df909211c12dc2d5490876948e37f9e61b747936
Rares <rarespepsi@gmail.com>
2026-02-16 01:14:05 +0200

    fix static files

--- adoptapet_pro/platforma/settings.py
+++ adoptapet_pro/platforma/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (uploads - poze animale etc.)
 # Pe Render: disco ephemeral, folosește /tmp (pozele se pierd la redeploy)
